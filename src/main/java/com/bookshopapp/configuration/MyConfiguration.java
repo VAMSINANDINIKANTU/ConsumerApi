d35cc7eef3c5a2b6fb3251c754430aa60d7430f6
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-05-09 21:41:52 +0530

    first commit

--- src/main/java/com/bookshopapp/configuration/MyConfiguration.java
+++ src/main/java/com/bookshopapp/configuration/MyConfiguration.java
@@ -1,11 +1,9 @@
 package com.bookshopapp.configuration;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.bookshopapp.service.ProviderConnector;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
